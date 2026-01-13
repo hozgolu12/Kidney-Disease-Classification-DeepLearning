70eb2fc566ed45170a1be3ef5eb0d6327b46df42
hozgolu12 <hozmha@gmail.com>
2026-01-13 19:37:49 +0530

    update readme.md and automate copying from artifacts/training model into model directory

--- src/cnnClassifier/pipeline/stage_03_model_training.py
+++ src/cnnClassifier/pipeline/stage_03_model_training.py
@@ -1,6 +1,8 @@
 from cnnClassifier.config.configuration import ConfigurationManager
 from cnnClassifier import logger
 from cnnClassifier.components.model_training import Training
+import os
+import shutil
 
 STAGE_NAME = "Model Training Stage"
 
@@ -15,6 +17,12 @@ class ModelTrainingPipeline:
         training.get_base_model()
         training.train_valid_generator()
         training.train()
+
+        # Copying the model to the model directory for easy access
+        target_dir = "model"
+        os.makedirs(target_dir, exist_ok=True)
+        shutil.copy(training_config.trained_model_path, os.path.join(target_dir, "trained_model.h5"))
+        logger.info(f"Trained model copied to {target_dir}/trained_model.h5")
 
 
 if __name__ == '__main__':
